cd1ecd8ca2ece6cc1b3e5f0f400f9f20df71051d
yldrmahmet <ay689089@gmail.com>
2026-04-14 00:06:21 +0300

    add part2c with learning json-server and axios

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,42 +1,52 @@
-import { useState } from 'react'
-import Note from './components/Note'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState("a new note")
-  const [showAll, setShowAll] = useState(true)
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("a new note");
+  const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log(response.data);
+      setNotes(response.data);
+    });
+  };
+  
+  useEffect(hook, []);
+  console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
       id: String(notes.length + 1),
-    }
+    };
 
-    setNotes([...notes, noteObject])
-    setNewNote('')
-  }
+    setNotes([...notes, noteObject]);
+    setNewNote("");
+  };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
 
   const handleShowImportantClick = () => {
-    setShowAll(!showAll)
-  }
+    setShowAll(!showAll);
+  };
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   return (
     <div>
       <h1>Notes</h1>
       <div>
         <button onClick={handleShowImportantClick}>
-          show  {showAll ? "important" : "all"}
+          show {showAll ? "important" : "all"}
         </button>
       </div>
       <ul>
@@ -46,10 +56,10 @@ const App = (props) => {
       </ul>
       <form onSubmit={addNote}>
         <input type="text" value={newNote} onChange={handleNoteChange} />
-        <button type='submit'>save</button>
+        <button type="submit">save</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,65 +1,86 @@
-import { useState } from "react"
-import Filter from "./components/Filter"
-import PersonForm from "./components/PersonForm"
-import Persons from "./components/Persons"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState("")
-  const [newNumber, setNewNumber] = useState("")
-  const [filter, setFilter] = useState("")
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const hook = () => {
+    console.log("effect started");
+    
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  };
+
+  useEffect(hook, []);
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-
-  }
+    setFilter(event.target.value);
+  };
 
   const handlePersonClick = (event) => {
-    event.preventDefault()
-    const trimmedName = newName.trim()
-    const trimmedNumber = newNumber.trim()
+    event.preventDefault();
+    const trimmedName = newName.trim();
+    const trimmedNumber = newNumber.trim();
     if (!trimmedName || !trimmedNumber) {
-      alert("blank name or blank number")
-      return
+      alert("blank name or blank number");
+      return;
     }
     // don't add person with same name or same number
-    const samePerson = persons.find((person) => person.name === trimmedName || person.number === trimmedNumber)
+    const samePerson = persons.find(
+      (person) =>
+        person.name === trimmedName || person.number === trimmedNumber,
+    );
     if (samePerson) {
-      alert(`${samePerson.name} ${samePerson.number} is already added to phonebook`)
-      return
+      alert(
+        `${samePerson.name} ${samePerson.number} is already added to phonebook`,
+      );
+      return;
     }
     // add new person
-    setPersons([...persons, { name: trimmedName, number: trimmedNumber, id: persons.length + 1 }])
-  }
+    setPersons([
+      ...persons,
+      { name: trimmedName, number: trimmedNumber, id: persons.length + 1 },
+    ]);
+  };
 
   // search input logic for filtering
   const contacts = filter
-    ? persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()) || person.number.includes(filter))
-    : persons
+    ? persons.filter(
+        (person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase()) ||
+          person.number.includes(filter),
+      )
+    : persons;
 
   return (
     <div>
       <h2>phonebook</h2>
       <Filter onChange={handleFilterChange} />
       <h3>add a new</h3>
-      <PersonForm onSubmit={handlePersonClick} onNameChange={handleNameChange} onNumberChange={handleNumberChange} />
+      <PersonForm
+        onSubmit={handlePersonClick}
+        onNameChange={handleNameChange}
+        onNumberChange={handleNumberChange}
+      />
       <h3>Numbers</h3>
       <Persons contacts={contacts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
