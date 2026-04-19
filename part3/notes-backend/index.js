d3ce723100bb5a83a7d94fedb78d3fe82a52f9ef
yldrmahmet <ay689089@gmail.com>
2026-04-19 18:42:15 +0300

    preparing render deploy

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -34,6 +34,9 @@ const requestLogger = (request, response, next) => {
 // 1.cors
 app.use(cors());
 
+// 2. 
+app.use(express.static("dist"));
+
 // 2. Content-type = application/json => JSON.parse()
 app.use(express.json());
 
