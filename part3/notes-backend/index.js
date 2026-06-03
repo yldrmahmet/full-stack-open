e00d56e5a03e58c707f8dc4bdbbe5aedec678206
yldrmahmet <ay689089@gmail.com>
2026-06-03 15:14:10 +0300

    add mongoose validation

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -50,19 +50,18 @@ app.delete("/api/notes/:id", (request, response, next) => {
 });
 
 // create one note
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({ error: "content missing" });
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => response.json(savedNote));
+  note
+    .save()
+    .then((savedNote) => response.json(savedNote))
+    .catch((error) => next(error));
 });
 
 // update one note
@@ -94,6 +93,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
