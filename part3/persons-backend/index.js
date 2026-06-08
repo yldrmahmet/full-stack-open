005e27a6f9cd550714261ee900f8907111403081
yldrmahmet <ay689089@gmail.com>
2026-06-08 15:28:05 +0300

    uibuild

--- part3/persons-backend/index.js
+++ part3/persons-backend/index.js
@@ -52,12 +52,6 @@ app.delete("/api/persons/:id", (request, response, next) => {
 app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
-  if (!body.name || !body.number) {
-    return response
-      .status(400)
-      .json({ error: "the name or number is missing" });
-  }
-
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -98,6 +92,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
--- part3/persons-backend/models/person.js
+++ part3/persons-backend/models/person.js
@@ -12,8 +12,23 @@ mongoose
   );
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    maxLength: 15,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return /^\d{2,3}-\d+$/.test(value);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
